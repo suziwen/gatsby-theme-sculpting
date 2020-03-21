9214ad1feaa31bbf4396b6afb890d8e81fe10ff6
suziwen <suziwen1@gmail.com>
2020-03-21 21:52:16 +0800

    zoom css font

---
+++ theme/gatsby-ssr.js
@@ -0,0 +1,20 @@
+const React = require('react')
+const {withPrefix} = require('gatsby')
+
+exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }, pluginOptions) => {
+  let output = [
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "libs/zoom/zoom.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "css/story-writer-markdown-default.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "css/story-writer-markdown-code-solarized-light.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "css/story-writer-markdown-components.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "fonts/Webfonts/xsj/stylesheet.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "xsjfonts/fontstyles/zh.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "css/font-awesome.min.css"}/>,
+    <link rel="stylesheet" type="text/css" href={withPrefix('/') + "css/svg-with-js.css"}/>
+  ]
+  setHeadComponents(output)
+  setPostBodyComponents([
+    <script src={withPrefix('/') + "libs/zoom/zoom-vanilla.min.js"}></script>,
+  ])
+}
+
