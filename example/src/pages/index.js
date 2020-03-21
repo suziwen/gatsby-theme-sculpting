34608fff00492eaaaee57cd69357386a3773059c
suziwen <suziwen1@gmail.com>
2020-03-21 10:29:33 +0800

    add theme-ui

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,3 +1,11 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import React from "react"
 
-export default () => <div>Homepage in a user's site</div>
+export default () => <div
+  sx={{
+    padding: 4,
+    color: 'background',
+    backgroundColor: 'primary'
+  }}
+  >Homepage in a user's site</div>
