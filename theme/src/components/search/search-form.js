f679666c81c6a7fb5639e92edf5af7f3fe03f87f
suziwen <suziwen1@gmail.com>
2020-03-25 21:47:56 +0800

    搜索

--- theme/src/components/search/search-form.js
+++ theme/src/components/search/search-form.js
@@ -6,15 +6,21 @@ class SearchForm extends React.Component {
   constructor(props) {
     super(props)
 
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.state = {
       query: props.query
     }
   }
+
+  handleSubmit(event) {
+    navigate(`/search?keywords=${encodeURIComponent(this.state.query)}`)
+  }
+
   render() {
     const self = this;
     const { query } = this.state
     return (
-      <form role="search" method="GET">
+      <form role="search" method="GET" onSubmit={ this.handleSubmit }>
         <label htmlFor="search-input">
           <h1>Search posts</h1>
         </label>
@@ -25,9 +31,6 @@ class SearchForm extends React.Component {
           aria-controls="search-results-count"
           {...reactComposition({
             onChange: e =>{
-              if (e.reactComposition.composition === false) {
-                navigate(`/search?keywords=${encodeURIComponent(e.target.value)}`)
-              }
               self.setState({
                 query: e.target.value
               })
