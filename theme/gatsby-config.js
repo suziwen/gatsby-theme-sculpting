69c84264aeb310c4ac2a5779734c736d4810485a
suziwen <suziwen1@gmail.com>
2020-04-02 09:39:05 +0800

    author

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -181,6 +181,7 @@ module.exports = options => {
     siteMetadata: {
       title: "小书匠",
       siteUrl: "http://www.xiaoshujiang.com",
+      author: "suziwen",
       description: `一款让你爱不释手的写作软件`
     },
     plugins: plugins
