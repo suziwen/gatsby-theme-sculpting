1aa17df3534e26dc1aeeed154c13de120156cba5
suziwen <suziwen1@gmail.com>
2020-04-04 11:33:06 +0800

    title 通过 context 获取

--- theme/src/components/site-title.js
+++ theme/src/components/site-title.js
@@ -2,40 +2,29 @@
 import { jsx } from 'theme-ui'
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
+import ContextConsumer from "../components/context"
 
-const SiteMetadata = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            siteUrl
-            title
-          }
+const SiteMetadata = () => {
+  return (
+    <ContextConsumer>
+      {({siteMetadata: {title}}) => {
+        const style = {
+          ml: 3,
+          fontWeight: 'bold',
+          fontSize: '1.5em'
         }
-      }
-    `}
-    render={({
-      site: {
-        siteMetadata: { siteUrl, title },
-      },
-    }) => {
-      const style = {
-        ml: 3,
-        fontWeight: 'bold',
-        fontSize: '1.5em'
-      }
-      if (title === '小书匠') {
-        style['fontFamily'] = 'webfontxiaoshujiang, sans-serif'
-      }
-      return (
-        <div sx={style}>
-          {title}
-        </div>
-      )
-    }}
-  />
-)
+        if (title === '小书匠') {
+          style['fontFamily'] = 'webfontxiaoshujiang, sans-serif'
+        }
+        return (
+          <div sx={style}>
+            {title}
+          </div>
+        )
+      }}
+    </ContextConsumer>
+  )
+}
 
 export default SiteMetadata
 
