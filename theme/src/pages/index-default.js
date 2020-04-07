502a3c49319d62d23e0db4ba1141b2f74d510a63
suziwen <suziwen1@gmail.com>
2020-04-07 18:17:23 +0800

    说明文字

--- theme/src/pages/index-default.js
+++ theme/src/pages/index-default.js
@@ -44,9 +44,9 @@ const Home = ({ data, location }) => {
       }}>当您看到该页面时,表示已经成功安装了 Sculpting 主题的博客,接下来只需要完成下面的步骤,就可以查看自己的文章了.</p>
       <ol>
         <li><FaRegEdit /> 使用<a href="http://markdown.xiaoshujiang.com" target="_blank">小书匠编辑器</a> 撰写一篇文章</li>
-        <li><FaSignOutAlt /> 通过小书匠编辑器的<a href="http://soft.xiaoshujiang.com/docs/tutorial/import_and_export/" target="_blank">导出</a>功能,将刚撰写的文章导出成 zip 格式</li>
-        <li><FaSignInAlt /> 将导出的 zip 文章放置在博客安装目录下的 <code>posts</code> 文件夹里</li>
-        <li><FaHandSpock /> 等程序重新编译后,刷新网页,就可以在博客里查看该文章了</li>
+        <li><FaSignOutAlt /> 通过小书匠编辑器的<a href="http://soft.xiaoshujiang.com/docs/tutorial/import_and_export/" target="_blank">导出</a>功能,将刚撰写的文章导出成 zip 格式文件</li>
+        <li><FaSignInAlt /> 将导出的 zip 文件放置在博客安装目录下的 <code>posts</code> 文件夹里</li>
+        <li><FaHandSpock /> 重新编译 Sculpting,刷新网页,就可以在博客里查看新文章了</li>
         <li><FaLightbulb /> 更多个性化修改,可以参考<a href="https://github.com/suziwen/gatsby-theme-sculpting" target="_blank">这里</a></li>
       </ol>
     </Flex>
